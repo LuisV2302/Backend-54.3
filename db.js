50414b323215bf42d4aade2e092d3516871f55f1
DiekroCat <87589054+DiekroCat@users.noreply.github.com>
2021-10-14 06:52:56 -0500

    fin

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
     //url base de datos
-    url: "mongodb+srv://grupo3:Misintic543@cluster0.rsktw.mongodb.net/Medictorio?retryWrites=true&w=majority"
+    url:"mongodb+srv://WilliamAfton:carrotstrawberry@cluster0.ebp1j.mongodb.net/NeoMatrix?retryWrites=true&w=majority"
+//    url: "mongodb+srv://grupo3:Misintic543@cluster0.rsktw.mongodb.net/Medictorio?retryWrites=true&w=majority"
 }
